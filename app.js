a991c7ea4024dc750cca5ffd45802972cfd660ce
opulencedeveloper <victorkudos@gmail.com>
2023-06-05 19:22:19 +0100

    updated for students

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const rootDir = require('./util/path')
+const rootDir = require("./util/path");
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("view engine", "pug");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -21,7 +21,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 404})
+  res.status(404).render("404", { pageTitle: 404 });
 });
 
 app.listen(3000);
